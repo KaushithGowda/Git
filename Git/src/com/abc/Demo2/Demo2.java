35845c3e5bbeb4c1d809d19a8c2716386542804f
KaushithGowda <koushith7@gmail.com>
2021-04-25 13:35:44 +0530

    First commit

---
+++ Git/src/com/abc/Demo2/Demo2.java
@@ -0,0 +1,10 @@
+package com.abc.Demo2;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is Demo2");
+	}
+
+}
---
+++ Git/src/com/abc/Demo3/Demo3.java
@@ -0,0 +1,10 @@
+package com.abc.Demo3;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is Demo3");
+	}
+
+}
